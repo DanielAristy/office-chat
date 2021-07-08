76abd1923de1227d7b0ff166f5b1c332ce0a4d5f
DanielAristy <danielaristy22@gmail.com>
2021-07-07 19:25:05 -0500

    Separando por componentes y utlizarlos en los Route utilizando react-router-dom

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Contact from '../components/Contact'
+import Home from '../components/Home'
+import Login from '../components/Login'
 import Navbar from '../components/Navbar'
+import Register from '../components/Register'
 
 export default function AppRouter() {
     return (
         <Router>
             <Navbar />
             <Switch>
-                <Route path="/contact">
-                    <h1>Contactanos</h1>
-                </Route>
-                <Route path="/login">
-                    <h1>Login</h1>
-                </Route>
-                <Route path="/register">
-                    <h1>Registrarse</h1>
-                </Route>
-                <Route exact path="/">
-                    <h1>Home</h1>
-                </Route>
+                <Route path="/contact" component={Contact} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route exact path="/" component={Home} />
                 <Route path="/404">
                     <h1>404 Not Found</h1>
                 </Route>
