4a99b83a06de15fca13ff1672c9827641dbd635c
DanielAristy <danielaristy22@gmail.com>
2021-07-09 08:19:40 -0500

    Añadiendo estructura de la pestaña Contactanos

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
 
 export default function Contact() {
     return (
-        <div className="py-5 text-center container">
-            <h1>Contactanos</h1>
-        </div>
+        <section className="py-5 text-center container">
+            <div className="row py-lg-5">
+                <div className="col-lg-6 col-md-8 mx-auto">
+                    <h1 className="fw-light">Contactanos</h1>
+                    <p className="lead text-muted">
+                        Direccion: Calle 22 Nro 31 -12 Sector la Dalia Marinilla.<br></br>
+                        Telefonos: 3116350799 - 5484840.<br></br>
+                        Encargado: Guillermo Leon Aristizabal
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
